3704e65f310fcfd1ef0e5090bc347a5df07c5c4b
Pauline Pettersson <pauline-pettersson@hotmail.com>
2025-12-04 10:53:46 +0100

    adding an icon from fontawesome

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+
+let quizContainer = document.querySelector('.quiz-container');
+let userName = document.querySelector('#name').value;
+let playBtn = document.querySelector('.play');
+let nextBtn = document.querySelector('.next');
+
+
